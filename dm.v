714de25238adbf99e0d2fadf038b91738c2ce117
yinkehoujing <153069815+yinkehoujing@users.noreply.github.com>
2025-06-28 21:02:52 +0800

    准备阻塞特殊情况

--- dm.v
+++ dm.v
@@ -1,4 +1,3 @@
-
 `include "ctrl_encode_def.v"
 // data memory
 module dm (
@@ -11,16 +10,16 @@ module dm (
 );
   input clk;
   input DMWr;
-  input [2:0] DMType;
   input [8:0] addr;
   input [31:0] din;
+  input [2:0] DMType;
   output reg [31:0] dout;
 
-  reg [7:0] dmem[(1 << 9) - 1:0];
+  reg [7:0] dmem[255:0];
 
-  // 写操作（时钟上升沿）
   always @(posedge clk) begin
     if (DMWr) begin
+
       case (DMType)
         `dm_byte: dmem[addr] <= din[7:0];
         `dm_byte_unsigned: dmem[addr] <= din[7:0];
@@ -39,32 +38,22 @@ module dm (
           dmem[addr+3] <= din[31:24];
         end
       endcase
+      $display("dmem[0x%8X] = 0x%8X,", addr, dmem[addr]);
+      $display("dmem[0x%8X] = 0x%8X,", addr + 1, dmem[addr+1]);
+      $display("dmem[0x%8X] = 0x%8X,", addr + 2, dmem[addr+2]);
+      $display("dmem[0x%8X] = 0x%8X,", addr + 3, dmem[addr+3]);
     end
   end
-
   always @(*) begin
     case (DMType)
-      `dm_byte: begin
-        dout = {{24{dmem[addr][7]}}, dmem[addr][7:0]};
-      end
-      `dm_byte_unsigned: begin
-        dout = {24'b0, dmem[addr][7:0]};
-      end
-      `dm_halfword: begin
-        dout = {{16{dmem[addr+1][7]}}, dmem[addr+1][7:0], dmem[addr][7:0]};
-      end
-      `dm_halfword_unsigned: begin
-        dout = {16'b0, dmem[addr+1][7:0], dmem[addr][7:0]};
-      end
-      `dm_word: begin
-        dout = {dmem[addr+3][7:0], dmem[addr+2][7:0], dmem[addr+1][7:0], dmem[addr][7:0]};
-      end
-      default: begin
-        dout = 32'b0;
-      end
+      `dm_byte: dout = {{24{dmem[addr][7]}}, dmem[addr][7:0]};
+      `dm_byte_unsigned: dout = {24'b0, dmem[addr][7:0]};
+      `dm_halfword: dout <= {{16{dmem[addr+1][7]}}, dmem[addr+1][7:0], dmem[addr][7:0]};
+      `dm_halfword_unsigned: dout <= {16'b0, dmem[addr+1][7:0], dmem[addr][7:0]};
+      `dm_word:
+      dout <= {{dmem[addr+3][7:0]}, {dmem[addr+2][7:0]}, {dmem[addr+1][7:0]}, dmem[addr][7:0]};
+      default: dout <= 32'b0;
     endcase
   end
-
-
 
 endmodule
--- sccomp_tb.v
+++ sccomp_tb.v
@@ -23,7 +23,9 @@ module sccomp_tb ();
     // $readmemh("sim5.dat", U_SCCOMP.U_IM.ROM, 0, 26);  // load instructions into instruction memory
     // $readmemh("sim6.dat", U_SCCOMP.U_IM.ROM, 0, 
     // 192);
-      $readmemh("sim2_f.dat", U_SCCOMP.U_IM.ROM, 0, 23);
+      // $readmemh("sim1.dat", U_SCCOMP.U_IM.ROM, 0, 30);
+      // $readmemh("sim2_f.dat", U_SCCOMP.U_IM.ROM, 0, 23);
+      $readmemh("hhh.dat", U_SCCOMP.U_IM.ROM, 0, 2);
     // $monitor("PC = 0x%8X, instr = 0x%8X", U_SCCOMP.PC, U_SCCOMP.instr); // used for debug
     foutput = $fopen("results.txt");
     debug_output = $fopen("debug.txt");
