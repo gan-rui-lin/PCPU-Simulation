127feb00f5a638d3b029b4d2ea6d66e2187508f5
yinkehoujing <153069815+yinkehoujing@users.noreply.github.com>
2025-07-01 15:18:51 +0800

    显示宏

---
+++ macro.v
@@ -0,0 +1,11 @@
+`define ROM_DISP 6'b100000
+`define RF_DISP 6'b010000
+`define ALU_DISP 6'b001000
+`define DM_DISP 6'b000100
+`define IMM_DISP 6'b000010
+`define PC_DISP 6'b000001
+
+`define ALU_SIZE 5
+`define ROM_SIZE 512
+`define RF_SIZE 32
+`define DM_SIZE 128
